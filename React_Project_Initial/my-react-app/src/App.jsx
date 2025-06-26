3cb961ac27dd9887b2946176ec49c1265bb02b82
Kishansavaliya22 <kishansavaliya22@gmail.com>
2025-06-26 11:40:40 +0530

    New Project Begin

---
+++ React_Project_Initial/my-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold text-blue-600">Hi Kishan</h1>
+      <button className="text-white px-4 sm:px-8 py-0 bg-sky-700 hover:bg-sky-800">Submit</button>
+    </>
+  )
+}
+
+export default App

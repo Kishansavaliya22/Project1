423d1e603b849bd8106a63dec349a4ebf488ebb2
Kishansavaliya22 <kishansavaliya22@gmail.com>
2025-06-30 10:47:55 +0530

    Change in Board component return value

--- Tic-tac-toe/src/App.jsx
+++ Tic-tac-toe/src/App.jsx
@@ -29,34 +29,24 @@ function Board({xIsNext , squares, onplay}){
     status = "Next Player: " + (xIsNext ? "X" : "O"); 
   }
 
-  // const Loop = for (let i = 0; i < 3; i++) {
-  //       for (let i = 0; i < 3; i++) {
-  //         return <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
-  // }
-  // }
-
-  // return(
-  //     Loop
-  // )
+//   const squareBoxElement = () =>  {
+//     let myArray = []
+//     for (let i = 0; i < 3; i++) {
+//       for (let i = 0; i < 3; i++) {
+//         myArray.push(<Square value={squares[i]} onSquareClick={() => handleClick(i)} />) 
+//       }
+//     }
+//     return myArray
+// }
 
   return(
     <>
-    <div className='status'>{status}</div>
-    <div className="box-square">
-      <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-      <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-      <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-    </div>
-    <div className="box-square">
-      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-    </div>
-    <div className="box-square">
-      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-    </div>
+      <div className='status'>{status}</div>
+      <div className="box-square">{
+        [...new Array(9)].map((item , index) => (
+          <Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} />
+        ))
+        }</div>
     </>
   )
 }

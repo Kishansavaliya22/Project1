5bb2d51bbe7c474ed32e5412700a8cb8c8b7ea1d
Kishansavaliya22 <kishansavaliya22@gmail.com>
2025-07-02 15:05:05 +0530

    UI Design and Component making

--- Design Agency/src/App.jsx
+++ Design Agency/src/App.jsx
@@ -1,19 +1,18 @@
 // import React from 'react';
 // import { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css'
-import Header from './pages/Component/Header';
+import "./App.css";
+import Header from "./pages/Component/Header";
 
-export  default function App() {
+export default function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
       <Routes>
-          <Route path='/' element={<Header />}></Route>
+        <Route path="/" element={<Header />}></Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
--- Design Agency/src/pages/Component/Header.jsx
+++ Design Agency/src/pages/Component/Header.jsx
@@ -5,20 +5,20 @@ export default function Header() {
     <header class="flex flex-row justify-between items-stretch font-[Poppins]">
       <div class="font-bold text-2xl tracking-normal">
         <a href="#">
-          <img
-            class="max-w-fit"
-            src="../../../public/images/logo.png"
-            alt="Design Agency"
-          />
+          <img class="max-w-fit" src="/images/logo.png" alt="Design Agency" />
         </a>
       </div>
       <nav>
-        <ul class="flex font-medium text-lg tracking-normal text-black">
-          <li class="px-2">Services</li>
-          <li class="px-2">About Us</li>
-          <li class="px-2">Contact Us</li>
-          <li class="px-2 border-solid border-black">Login</li>
-          <li class="px-2">Register</li>
+        <ul class="flex font-medium text-lg tracking-normal text-black gap-x-8">
+          <li class="px-3 w-33">Services</li>
+          <li class="px-3 w-26">About Us</li>
+          <li class="px-3 w-31">Contact Us</li>
+          <li class="px-3 border border-[#20B15A] border-solid rounded-lg w-26">
+            Login
+          </li>
+          <li class="px-3 border border-[#20B15A] border-solid rounded-lg bg-[#20B15A] w-30">
+            Register
+          </li>
         </ul>
       </nav>
     </header>
